5ba3ccb4235b7f7310a428e192a394655b5bda4a
theperryr <theperryr@gmail.com>
2019-02-21 12:45:51 -0600

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,16 +6,9 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  <div style={{display: 'flex', alignItems: 'center', width: '100vw', height: '100vh'}}>
+    <img src='https://media3.giphy.com/media/l2YOp5fNAFMp63B3a/giphy_s.gif?cid=3640f6095c6ef0f8586b4563679688cb'/>
+  </div>
 )
 
 export default IndexPage
